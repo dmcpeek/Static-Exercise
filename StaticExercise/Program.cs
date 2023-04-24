25e5bd3718ba7f0a0b3b6135d91d0c2e38d1b45d
Doug McPeek <dmcpeek@outlook.com>
2023-04-24 11:33:22 -0600

    First check in

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double farenheitTemp = 32;
+            var celsius = TempConverter.FahrenheitToCelsius(32); // Expect 0° returned
+            Console.WriteLine($"{farenheitTemp}° Farenheit = {celsius}° Celsius");
+
+            double celsiusTemp = 0;
+            var farenheit = TempConverter.CelsiusToFarenheit(0); // Expect 32° returned
+            Console.WriteLine($"{celsiusTemp}° Celsius = {farenheit}° Farenheit");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double farenheit) 
+        {
+            return ((farenheit - 32) / 1.8);
+        }
+
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            return ((celsius * 9) + 32);
+        }
+    }
+}
